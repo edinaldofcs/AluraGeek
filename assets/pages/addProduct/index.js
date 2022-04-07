c1fef89b4b3a4f838c76ed4ebbd702ddf5068aef
Edinaldo Fernandes Cardoso Santos <fernandescro5@gmail.com>
2022-04-07 12:41:34 -0300

    Mensagens na tela e refatoração

--- assets/pages/addProduct/index.js
+++ assets/pages/addProduct/index.js
@@ -2,7 +2,57 @@ import { lista } from '../../js/fakeAPI/index.js';
 import { updateList } from '../../js/index.js';
 import { footer, nav } from '../../js/navHeader/index.js'
 
+function ExibirMensagem(classe) {
+  const msg = document.querySelector('[data-msg]');
+  if (classe == "msg--enviar") {
+    msg.innerHTML = "Cadastrado com sucesso";
+  } else {
+    msg.innerHTML = "Existem Campos em branco";
+  }
+  
+  msg.classList.add(classe);
+  setTimeout(() => {
+    msg.classList.remove(classe);
+  }, 1500);
+}
+
+function escolherId(titulo) {
+  updateList();
+  var id = 0;
+  for (let i = 0; i < lista.length; i++) {
+    if (lista[i].titulo.toLowerCase() == titulo.value.toLowerCase()) {
+      id = lista[i].produtos[lista[i].produtos.length - 1].infos.id + 1
+      break;
+    }
+  }
+
+  if(id === 0){
+    id = 1;
+  }
+  return id;
+}
+
+function limparCampos() {
+  document.getElementById('categoria').value = '';
+  document.getElementById('alt').value = '';
+  document.getElementById('nome').value = '';
+  document.getElementById('preco').value = '';
+  document.getElementById('descricao').value = '';
+  const file = document.querySelector("[data-imgFile]");
+  file.textContent = '';
+  file.classList.remove('show');
+}
+
 window.onload = function () {
+
+  const titulo = document.getElementById('categoria');
+  const alt = document.getElementById('alt');
+  const nome = document.getElementById('nome');
+  const preco = document.getElementById('preco');
+  const descricao = document.getElementById('descricao');
+  const img = document.querySelectorAll("#select");;
+  const file = document.querySelector("[data-imgFile]");
+  const enviar = document.querySelector('[data-send]');
 
   const paths = {
     controle: "../../img/controle.png",
@@ -18,89 +68,62 @@ window.onload = function () {
   nav(paths);
   footer(paths);
 
-  const img = document.querySelectorAll("#select");
   var imgPath = null;
-  const file = document.querySelector("[data-imgFile]")
 
   img.forEach((inputFile) => {
     inputFile.addEventListener('change', (e) => {
       var fReader = new FileReader();
       fReader.readAsDataURL(inputFile.files[0]);
       fReader.onloadend = function (event) {
-        imgPath = event.target.result
-        file.textContent = inputFile.files[0].name
-        file.classList.add('show')
+        imgPath = event.target.result;
+        file.textContent = inputFile.files[0].name;
+        file.classList.add('show');
       }
     })
   })
-  
-  const titulo = document.getElementById('categoria')
-  const nome = document.getElementById('nome')
-  const preco = document.getElementById('preco')
-  const descricao = document.getElementById('descricao')
-  const formArea = [titulo, nome, preco, descricao]
 
-  const enviar = document.querySelector('[data-send]')
+
   enviar.addEventListener('click', (e) => {
     e.preventDefault();
+    const formArea = [titulo.value, nome.value, preco.value, descricao.value, alt.value, imgPath];
     var send = true;
-    formArea.forEach(item => {
-      if (!item.value) {
+
+    for (let i in formArea) {
+      if (!formArea[i]) {
+        ExibirMensagem('msg--erro');
         send = false;
-        alert("Campos inválidos")
+        break;
       }
-    })
-    if (!imgPath) {
-      alert('sem nada');
-      send = false;
     }
 
+
     if (send) {
-      updateList();
-      var idTemp = 0;
-      var unique = true
-      const num = lista.filter(lastId)
-
-      function lastId(item) {
-        if (item.titulo.toLowerCase() == titulo.value.toLowerCase()) {
-          idTemp = item.produtos[item.produtos.length - 1].infos.id + 1
-          unique = false;
-        }
-      }
-
-      if (unique) {
-        idTemp = 1;
-      }
-
-      // console.log(idTemp)
+      var newId = escolherId(titulo);
 
       const novoProduto = {
         titulo: titulo.value,
         produtos: [{
           infos: {
-            id: idTemp,
+            id: newId,
             nome: nome.value,
             img: imgPath,
             preco: Number(preco.value).toFixed(2),
-            descricao: descricao.value
+            descricao: descricao.value,
+            alt: alt.value,
           }
         }]
       }
 
-      const produto = JSON.parse(localStorage.getItem('lista')) || []
+      const produto = JSON.parse(localStorage.getItem('lista')) || [];
+      const add = [...produto, novoProduto];
+      localStorage.setItem('lista', JSON.stringify(add));
 
-      const add = [...produto, novoProduto]
-      localStorage.setItem('lista', JSON.stringify(add))
+      ExibirMensagem('msg--enviar');
 
-      alert("Item adicionado com sucesso!");
-      titulo.value = ''
-      nome.value = ''
-      preco.value = ''
-      descricao.value = ''      
-      file.textContent = ''
-      file.classList.remove('show')
-
+      limparCampos();
     }
   })
 
 }
+
+
